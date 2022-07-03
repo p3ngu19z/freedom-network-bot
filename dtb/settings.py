2dc483ada92790cb2ce8ae389168328ac8e66783
p3ngu19z <penguinz@protonmail.com>
2022-07-03 19:57:11 +0300

    fix staticfiles

--- dtb/settings.py
+++ dtb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*",]  # since Telegram uses a lot of IPs for webhooks
 
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
